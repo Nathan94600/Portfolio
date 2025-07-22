2a2ed06dc2b8a0ebf54f55b03734626d87ed1287
Nathan94600 <nathan94nahtan@gmail.com>
2025-07-22 02:09:29 +0200

    Suppression du système de component pour les fichiers html et ajout de la compression des fichiers

--- server.js
+++ server.js
@@ -1,46 +1,97 @@
 const { createServer } = require("http"),
-{ readFile } = require("fs");
+{ readFile, readdir, writeFile } = require("fs"),
+{ brotliCompress, deflate, gzip } = require("zlib"),
+supportedEncoding = ["br", "gzip", "deflate", "*"],
+fileExts = {
+	br: ".br",
+	gzip: ".gzip",
+	deflate: ".zip",
+	null: ""
+},
+fileExtRegex = /\.(br|zip|gzip)$/;
+defaultHeaders = {
+	HTML: {
+		"content-type": "text/html",
+		"content-language": "fr",
+		"content-security-policy": "default-src 'self'; img-src 'self' https://cdn.jsdelivr.net;"
+	},
+	CSS: {
+		"content-type": "text/css"
+	}
+};
 
-/**
- * @param { string } string
- */
-async function resolveComponent(string) {
-	return new Promise((resolve, reject) => {
-		const matchs = string.match(/(?<!\\)(\\\\)*{{[a-zA-Z]*}}/g);
+function compressDir(dirPath) {
+	readdir(dirPath, (err, files) => {		
+		if (err) console.log(`[compressDir] readdir (${dirPath})`);
+		else files.filter(file => !(/\.(br|zip|gzip)$/.test(file))).forEach(file => readFile(`${dirPath}/${file}`, (err, data) => {			
+			if (err) console.log(`[compressDir] readFile (${dirPath}/${file})`);
+			else {		
+				brotliCompress(data, (err, res) => {
+					if (err) console.log(`[compressDir] brotliCompress (${dirPath}/${file})`);
+					else writeFile(`${dirPath}/${file}.br`, res, err => {
+						if (err) console.log(`[compressDir] writeFile brotliCompress (${dirPath}/${file})`);
+					});
+				});
 
-		if (!matchs || matchs.length == 0) resolve(string);
-		else matchs.forEach((match, i) => readFile(`./components/${match.replace(/[\\{}]/g, "")}.html`, (err, data) => {			
-			if (err) reject(err);
-			else resolveComponent(string.replace(match, data)).then(res => {
-				string = res;
+				deflate(data, (err, res) => {
+					if (err) console.log(`[compressDir] deflate (${dirPath}/${file})`);
+					else writeFile(`${dirPath}/${file}.zip`, res, err => {
+						if (err) console.log(`[compressDir] writeFile deflate (${dirPath}/${file})`);
+					});
+				});
 
-				if (matchs.length - 1 == i) resolve(string);
-			}, reason => reject(reason));
+				gzip(data, (err, res) => {
+					if (err) console.log(`[compressDir] gzip (${dirPath}/${file})`);
+					else writeFile(`${dirPath}/${file}.gzip`, res, err => {
+						if (err) console.log(`[compressDir] writeFile gzip (${dirPath}/${file})`);
+					});
+				});
+			};
 		}));
 	});
 };
 
+compressDir("./javascript");
+compressDir("./pages");
+compressDir("./styles");
+
+/**
+ * @param { string } header Accept-Encoding
+ * @returns { string | null }
+ */
+function chooseEncoding(header) {
+	const encoding = header
+		.split(",")
+		.map(v => {
+			const [enc, qV] = v.trim().split(";q=");
+
+			return [enc, parseFloat(qV || "1")];
+		})
+		.filter(v => supportedEncoding.includes(v[0]))
+		.sort((a, b) => (b[1] - a[1]) || supportedEncoding.indexOf(a[0]) - supportedEncoding.indexOf(b[0]))[0]?.[0];
+
+	return encoding ? encoding == "*" ? "br" : encoding : null;
+};
+
 createServer((req, res) => {
+	const encoding = req.headers["accept-encoding"] ? chooseEncoding(req.headers["accept-encoding"]) : null;
+
+	if (encoding) {
+		defaultHeaders.HTML["content-encoding"] = encoding;
+		defaultHeaders.CSS["content-encoding"] = encoding;
+	};
+	
 	switch (req.url) {
 		// pages
 		case "/":
 			switch (req.method) {
 				case "GET":
-					readFile("./pages/index.html", (err, data) => {
+					readFile(`./pages/index.html${fileExts[encoding]}`, (err, data) => {
 						if (err) {
 							console.log("GET /", err);
 							
-							res.writeHead(500).end()
-						} else resolveComponent(data.toString()).then(str => res.writeHead(200, {
-							"content-type": "text/html",
-							"content-length": Buffer.from(str).length,
-							"content-language": "fr",
-							"content-security-policy": "default-src 'self'; img-src 'self' https://cdn.jsdelivr.net;"
-						}).end(str), reason => {
-							console.log("GET / (components)", reason);
-							
-							res.writeHead(500).end()
-						});
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.HTML, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
@@ -51,21 +102,12 @@ createServer((req, res) => {
 		case "/bts-sio":
 			switch (req.method) {
 				case "GET":
-					readFile("./pages/bts-sio.html", (err, data) => {
+					readFile(`./pages/bts-sio.html${fileExts[encoding]}`, (err, data) => {
 						if (err) {
 							console.log("GET /", err);
 							
-							res.writeHead(500).end()
-						} else resolveComponent(data.toString()).then(str => res.writeHead(200, {
-							"content-type": "text/html",
-							"content-length": Buffer.from(str).length,
-							"content-language": "fr",
-							"content-security-policy": "default-src 'self'; img-src 'self' https://cdn.jsdelivr.net;"
-						}).end(str), reason => {
-							console.log("GET / (components)", reason);
-							
-							res.writeHead(500).end()
-						});
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.HTML, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
@@ -76,21 +118,12 @@ createServer((req, res) => {
 		case "/contact":
 			switch (req.method) {
 				case "GET":
-					readFile("./pages/contact.html", (err, data) => {
+					readFile(`./pages/contact.html${fileExts[encoding]}`, (err, data) => {
 						if (err) {
 							console.log("GET /", err);
 							
-							res.writeHead(500).end()
-						} else resolveComponent(data.toString()).then(str => res.writeHead(200, {
-							"content-type": "text/html",
-							"content-length": Buffer.from(str).length,
-							"content-language": "fr",
-							"content-security-policy": "default-src 'self'; img-src 'self' https://cdn.jsdelivr.net;"
-						}).end(str), reason => {
-							console.log("GET / (components)", reason);
-							
-							res.writeHead(500).end()
-						});
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.HTML, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
@@ -101,21 +134,12 @@ createServer((req, res) => {
 		case "/epreuve-e5":
 			switch (req.method) {
 				case "GET":
-					readFile("./pages/epreuve-e5.html", (err, data) => {
+					readFile(`./pages/epreuve-e5.html${fileExts[encoding]}`, (err, data) => {
 						if (err) {
 							console.log("GET /", err);
 							
-							res.writeHead(500).end()
-						} else resolveComponent(data.toString()).then(str => res.writeHead(200, {
-							"content-type": "text/html",
-							"content-length": Buffer.from(str).length,
-							"content-language": "fr",
-							"content-security-policy": "default-src 'self'; img-src 'self' https://cdn.jsdelivr.net;"
-						}).end(str), reason => {
-							console.log("GET / (components)", reason);
-							
-							res.writeHead(500).end()
-						});
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.HTML, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
@@ -126,21 +150,12 @@ createServer((req, res) => {
 		case "/epreuve-e6":
 			switch (req.method) {
 				case "GET":
-					readFile("./pages/epreuve-e6.html", (err, data) => {
+					readFile(`./pages/epreuve-e6.html${fileExts[encoding]}`, (err, data) => {
 						if (err) {
 							console.log("GET /", err);
 							
-							res.writeHead(500).end()
-						} else resolveComponent(data.toString()).then(str => res.writeHead(200, {
-							"content-type": "text/html",
-							"content-length": Buffer.from(str).length,
-							"content-language": "fr",
-							"content-security-policy": "default-src 'self'; img-src 'self' https://cdn.jsdelivr.net;"
-						}).end(str), reason => {
-							console.log("GET / (components)", reason);
-							
-							res.writeHead(500).end()
-						});
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.HTML, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
@@ -151,21 +166,12 @@ createServer((req, res) => {
 		case "/veille-technologique":
 			switch (req.method) {
 				case "GET":
-					readFile("./pages/veille-technologique.html", (err, data) => {
+					readFile(`./pages/veille-technologique.html${fileExts[encoding]}`, (err, data) => {
 						if (err) {
 							console.log("GET /", err);
 							
-							res.writeHead(500).end()
-						} else resolveComponent(data.toString()).then(str => res.writeHead(200, {
-							"content-type": "text/html",
-							"content-length": Buffer.from(str).length,
-							"content-language": "fr",
-							"content-security-policy": "default-src 'self'; img-src 'self' https://cdn.jsdelivr.net;"
-						}).end(str), reason => {
-							console.log("GET / (components)", reason);
-							
-							res.writeHead(500).end()
-						});
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.HTML, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
@@ -177,15 +183,12 @@ createServer((req, res) => {
 		case "/styles/index.css":
 			switch (req.method) {
 				case "GET":
-					readFile("./styles/index.css", (err, data) => {
+					readFile(`./styles/index.css${fileExts[encoding]}`, (err, data) => {
 						if (err) {
 							console.log("GET /styles/index.css", err);
 							
-							res.writeHead(500).end()
-						} else res.writeHead(200, {
-							"content-type": "text/css",
-							"content-length": data.length
-						}).end(data);
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.CSS, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
@@ -196,15 +199,12 @@ createServer((req, res) => {
 		case "/styles/contact.css":
 			switch (req.method) {
 				case "GET":
-					readFile("./styles/contact.css", (err, data) => {
+					readFile(`./styles/contact.css${fileExts[encoding]}`, (err, data) => {
 						if (err) {
 							console.log("GET /styles/contact.css", err);
 							
-							res.writeHead(500).end()
-						} else res.writeHead(200, {
-							"content-type": "text/css",
-							"content-length": data.length
-						}).end(data);
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.CSS, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
@@ -215,15 +215,12 @@ createServer((req, res) => {
 		case "/styles/epreuve-e5.css":
 			switch (req.method) {
 				case "GET":
-					readFile("./styles/epreuve-e5.css", (err, data) => {
+					readFile(`./styles/epreuve-e5.css${fileExts[encoding]}`, (err, data) => {
 						if (err) {
 							console.log("GET /styles/epreuve-e5.css", err);
 							
-							res.writeHead(500).end()
-						} else res.writeHead(200, {
-							"content-type": "text/css",
-							"content-length": data.length
-						}).end(data);
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.CSS, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
@@ -234,15 +231,12 @@ createServer((req, res) => {
 		case "/styles/epreuve-e6.css":
 			switch (req.method) {
 				case "GET":
-					readFile("./styles/epreuve-e6.css", (err, data) => {
+					readFile(`./styles/epreuve-e6.css${fileExts[encoding]}`, (err, data) => {
 						if (err) {
 							console.log("GET /styles/epreuve-e6.css", err);
 							
-							res.writeHead(500).end()
-						} else res.writeHead(200, {
-							"content-type": "text/css",
-							"content-length": data.length
-						}).end(data);
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.CSS, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
@@ -253,15 +247,12 @@ createServer((req, res) => {
 		case "/styles/veille-technologique.css":
 			switch (req.method) {
 				case "GET":
-					readFile("./styles/veille-technologique.css", (err, data) => {
+					readFile(`./styles/veille-technologique.css${fileExts[encoding]}`, (err, data) => {
 						if (err) {
 							console.log("GET /styles/veille-technologique.css", err);
 							
-							res.writeHead(500).end()
-						} else res.writeHead(200, {
-							"content-type": "text/css",
-							"content-length": data.length
-						}).end(data);
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.CSS, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
@@ -272,15 +263,12 @@ createServer((req, res) => {
 		case "/styles/bts-sio.css":
 			switch (req.method) {
 				case "GET":
-					readFile("./styles/bts-sio.css", (err, data) => {
+					readFile(`./styles/bts-sio.css${fileExts[encoding]}`, (err, data) => {
 						if (err) {
 							console.log("GET /styles/bts-sio.css", err);
 							
-							res.writeHead(500).end()
-						} else res.writeHead(200, {
-							"content-type": "text/css",
-							"content-length": data.length
-						}).end(data);
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.CSS, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
@@ -288,18 +276,15 @@ createServer((req, res) => {
 					break;
 			}
 			break;
-		case "/styles/components/navbar.css":
+		case "/styles/navbar.css":
 			switch (req.method) {
 				case "GET":
-					readFile("./styles/components/navbar.css", (err, data) => {
+					readFile(`./styles/navbar.css${fileExts[encoding]}`, (err, data) => {
 						if (err) {
-							console.log("GET /styles/components/navbar.css", err);
+							console.log("GET /styles/navbar.css", err);
 							
-							res.writeHead(500).end()
-						} else res.writeHead(200, {
-							"content-type": "text/css",
-							"content-length": data.length
-						}).end(data);
+							res.writeHead(500).end();
+						} else res.writeHead(200, { ...defaultHeaders.CSS, "content-length": data.length }).end(data);
 					});
 					break;
 				default:
